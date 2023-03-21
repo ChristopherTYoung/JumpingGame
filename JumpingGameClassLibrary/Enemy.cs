9f597dbd91a6a58524b218dae2f8baf0f5801ae4
natera88 <58490296+natera88@users.noreply.github.com>
2023-03-20 18:11:02 -0600

    Added the enemy class

---
+++ JumpingGameClassLibrary/Enemy.cs
@@ -0,0 +1,13 @@
+using System.CodeDom.Compiler;
+
+namespace JumpingGameClassLibrary
+{
+    public class Enemy : IGame
+    {
+        public int[] Generate(string[] Icon)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
